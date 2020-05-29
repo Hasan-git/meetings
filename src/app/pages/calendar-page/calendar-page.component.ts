c8d76c2f0e36548faaacebec3dec5194b0c075d6
Hasan-git <hasan.ri25@gmail.com>
2020-05-29 09:56:15 +0300

    - End meeting - Recurrent events - Disabled voting

--- src/app/pages/calendar-page/calendar-page.component.ts
+++ src/app/pages/calendar-page/calendar-page.component.ts
@@ -54,6 +54,7 @@ export class CalendarPageComponent {
     action: string;
     event: CalendarEvent;
   };
+  recurrent: boolean = false
 
   topics: Topic[] = []
 
@@ -278,6 +279,9 @@ export class CalendarPageComponent {
       this.filenames = [];
       this.selectedItems_Rooms = [];
       this.selectedItems_Users = [];
+      this.topics = [];
+      this.recurrent = false;
+
       Swal.fire('SUCCESS', 'New event added successfully')
 
       this.myHelper.getEventsFromCalendar().subscribe((result: CalendarEvent[]) => {
@@ -520,6 +524,7 @@ export class CalendarPageComponent {
       topic: '',
       votes_up: [],
       votes_down: [],
+      votes_enabled:true,
     })
   }
 
